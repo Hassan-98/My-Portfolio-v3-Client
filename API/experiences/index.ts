72b766e7fa65b9e1e21c8dd24366ba05f0931504
Hassan Ali <7assan.3li1998@gmail.com>
2023-02-23 14:35:06 +0200

    update get experiences order

--- API/experiences/index.ts
+++ API/experiences/index.ts
@@ -3,12 +3,19 @@ import API from "../request.handler";
 import { IExperience } from './types';
 
 export async function getAllExperiences({ limit, cvOnly }: { limit?: number; cvOnly?: boolean; }): Promise<IExperience[] | undefined> {
-  let query = `?sort=endedAt,-startedAt`;
+  let query = `?sort=-endedAt`;
   if (limit) query += `&limit=${limit}`;
   if (cvOnly) query += `&showInCv=true`;
 
   const experience = await API.GET<IExperience[]>(`/experience${query}`);
-  if (experience) return experience;
+  if (experience) {
+    return experience.sort((a, b) => {
+      if (a.endedAt && b.endedAt) return new Date(b.endedAt).getTime() - new Date(a.endedAt).getTime();
+      else if (a.endedAt && !b.endedAt) return new Date(a.endedAt).getTime();
+      else if (!a.endedAt && b.endedAt) return - new Date(b.endedAt).getTime();
+      else return new Date(a.startedAt).getTime() - new Date(b.startedAt).getTime();
+    });
+  }
 }
 
 export async function getExperienceById(id: string): Promise<IExperience | undefined> {
